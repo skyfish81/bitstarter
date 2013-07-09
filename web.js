d0fcf19fb4f90a705399b1b616e74fdeeb9c89fb
Ryan Tan <skyfish81@gmail.com>
2013-07-09 17:43:32 +0000

    fix readfile

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var content = fs.readFile("index.html", "utf8");
+  var content = fs.readFileSync("index.html", "utf8");
   response.send(content);
 });
 
