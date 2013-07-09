1c12c059db8715d77fea5506afe415d43ef2d61e
Ryan Tan <skyfish81@gmail.com>
2013-07-09 17:32:15 +0000

    Add readFile from index.html

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var buf = fs.readFile('index.html');
+  response.send(buf.toString());
 });
 
 var port = process.env.PORT || 5000;
